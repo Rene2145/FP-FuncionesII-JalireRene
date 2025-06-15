b1612d14a8f266b6e0083909a559a8dc2cb8e2c9
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-06-15 18:05:01 -0500

    Ejercicio 3 - Cálculo de la hipotenusa, versión final

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include <cmath> 
 using namespace std;
+
+int main() {
+    double c1, c2, sc, h;
+	cout<<"CALCULO DE LA HIPOTENUSA"<<endl<<endl;
+    cout << "Escribe la longitud del primer cateto: "<<endl;
+    cin >> c1;
+
+    cout << "Escribe la longitud del segundo cateto: "<<endl;
+    cin >> c2;
+
+    sc = pow(c1, 2) + pow(c2, 2);
+    h = sqrt(sc);
+
+    cout << "El valor de la hipotenusa es:" <<endl<<h<< endl;
+
+    return 0;
+}
