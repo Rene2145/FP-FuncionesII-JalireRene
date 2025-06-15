8ba561e9252ed8593c0126b48acafab84b2f1582
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-06-15 15:09:51 -0500

    Ejercicio 1 – Cálculo del área de un círculo, versión final

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,16 +1,15 @@
-#include <iostream>
-#include "funciones.h"
- 
+#include <iostream>  
+#include <cmath>     
 using namespace std;
 
 int main() {
     double radio;
+	double area	;
+	cout<<"CALCULO DEL AREA DE UN CIRCULO"<<endl<<endl;
     cout << "Escribe el radio de tu circulo:"<<endl;
     cin >> radio;
-
-    double area = calcularArea(radio);
-    cout << "El area de tu circulo es: " << area << endl;
-
+    area = M_PI*pow(radio, 2); 
+    cout << "El area de tu circulo es:" <<endl<< area << endl;
     return 0;
 }
 
